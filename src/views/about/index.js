e524e3ff20035d01343c985303f7e5fddd3e1c2b
lzk <luozikun@aliyun.com>
2019-07-02 11:13:30 +0800

    git add

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -1,9 +1,109 @@
 import React, {Component} from 'react'
-
+import '@/styles/about/index.scss'
 class About extends Component {
   render () {
     return (
-      <h1>About</h1>
+      <div className='ui-page'>
+      <div className="top-banner">
+        <img src="//images.iqianjin.com/mimages/mobile/about/disclosure/banner.png" alt="" />
+      </div>
+      <div className="wrap-bg">
+        <div className='data-box1'>
+          <div className="data-box1-item01">爱钱进平台已经上线</div>
+          <div className="data-box1-item02">1883天</div>
+        </div>
+        <ul className="data-box2">
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="wrap-bg invest-wrap">
+        <h2>(截至2019-05-31)</h2>
+        <ul className="data-box2">
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+          <li>
+            <i>a</i>
+            <div className="cont-txt">
+              <span className="money">1,931.71亿元</span>
+              <span className="add">累计撮合交易额</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="wrap-bg invest-wrap"></div>
+      <div className="wrap-bg"></div>
+      <div className="wrap-bg"></div>
+      <div className="wrap-bg"></div>
+      <div className="link-area"></div>
+
+    </div>
     )
   }
 }
